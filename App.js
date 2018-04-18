587c88eab37d6605ef79b9056547ba941d283c5c
jcleigh <jcleigh@hy-vee.com>
2018-04-18 10:49:48 -0500

    test wip

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Hello there!</Text>
+	<Text>Tell your momma hi</Text>
       </View>
     );
   }
