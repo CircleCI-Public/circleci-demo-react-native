62f8cb6f281393f4c13100a11e2c74dcb3221b4c
Bachir Khiati <bachir@Bachirs-MacBook-Air.local>
2018-08-20 09:26:31 +0300

    sss

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello from CircleCI!</Text>
+        <Text>Hello frssom CircleCI!</Text>
       </View>
     );
   }
