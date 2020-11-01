c835a1d65e8fc46f8857bd8d1b1c19f0907c236f
wahyu setiawan <wahyu@gradana.com>
2020-11-01 18:08:39 +0700

    perbaikan wording

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello from CircleCI!</Text>
+        <Text>Hello from indonesia!</Text>
       </View>
     );
   }
@@ -14,8 +14,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
